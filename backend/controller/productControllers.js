cf2d63305cb4a3af65a5a31b32389d8d7f94cbb9
Ankhbayarbt <ankhaaa205@gmail.com>
2023-11-18 10:53:30 +0800

    setup and connect mongodb

---
+++ backend/controller/productControllers.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import Product from "../models/product";
+
+export const newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({ product });
+};
+
+export const getProducts = async (req, res, next) => {
+  // const products = await Product.find();
+  // res.status(200).json({ products });
+  return NextResponse.json({ products });
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    DB_URI: "mongodb://127.0.0.1:27017/ecommerce",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
