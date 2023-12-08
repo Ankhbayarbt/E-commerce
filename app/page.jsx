10fa2d74ecaacbf3daf49b990673d170e38b0ea8
Temuulen996 <temuulenuuree@gmail.com>
2023-12-08 16:14:32 +0800

    conflict zasah.

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,65 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import ListProducts from "@/components/products/ListProducts";
+import ListProducts from "@/components/clothes/list_clothes";
 import Image from "next/image";
+import { Carousel } from "flowbite-react";
+import CustomCarousel from "@/components/carousel";
+import ActiveSlider from "@/components/new_clothes_slider";
+import ClothesContext from "@/context/clothes_context";
+import UserContext from "@/context/user_context";
+import { useRouter } from "next/navigation";
+const HomePage = () => {
+  const clCtx = useContext(ClothesContext);
+  const usCtx = useContext(UserContext);
+  const [isLooggedIn, setLoggedIn] = useState(false);
+  const router = useRouter();
 
-const HomePage = async () => {
+  useEffect(() => {
+    //хуудас зурагдах үед uer, new item, clothes-ийн мэдээллүүдийг татан context-д хадгална.
+    usCtx.loadUsers();
+    clCtx.loadNewItems();
+    clCtx.loadClothes();
+    //хуудас ачааллах үед хэрэглэгч нэвтэрсэн эсэхийг шалгана.
+    usCtx
+      .authorization()
+      .then((res) => {
+        if (res) {
+          setLoggedIn(true);
+
+          if (res.user.role === "user") {
+            usCtx.setRole("user");
+            router.push("/");
+          } else {
+            usCtx.setRole("admin");
+          }
+        } else {
+          setLoggedIn(false);
+          router.push("/login");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="flex flex-col items-center w-full">
-      <div>
-        <img src="https://images.unsplash.com/photo-1540221652346-e5dd6b50f3e7?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      </div>
-      <div>suuld nemegsenguud</div>
+      {isLooggedIn ? (
+        <>
+          <div className="w-full my-6">
+            <CustomCarousel />
+          </div>
+          <div className="mb-6 text-5xl font-bold">
+            Шинээр нэмэгдсэн хувцаснууд
+          </div>
+          <div className="w-full">
+            <ActiveSlider />
+          </div>
+        </>
+      ) : (
+        <div></div>
+      )}
+
     </div>
   );
 };
